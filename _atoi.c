0c1fec557372834e7b7f53fb1832cbad4bf21e24
jgreat1221 <niyi4279@gmail.com>
2023-11-17 08:45:14 -0800

    project work

---
+++ _atoi.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * interactive  returns truee if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILEND) && info_>readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_deline(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - check for the alphabetic chraacter
+ * @c: the character to input
+ * Rteurn: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to be coverted
+ * Return: 0 if no number in string, cconverted nuumber otherwise
+ */
